7f8e901c9a5398c3ba9d982d21c60bc3845d97be
이됴 <iamdy@idyoui-iMac.local>
2022-03-28 15:43:19 +0900

    zz

---
+++ src/routers/userRouter.js
@@ -0,0 +1,14 @@
+ 
+import express from "express";
+import { edit, remove, logout } from "../controllers/userController";
+
+const userRouter = express.Router();
+
+
+userRouter.get("/logout", logout);
+userRouter.get("/edit", edit);
+userRouter.get("/remove", remove);
+
+
+
+export default userRouter;
---
+++ src/routers/videoRouter.js
@@ -0,0 +1,15 @@
+
+import express from "express";
+import { getEdit, postEdit, watch } from "../controllers/videoController";
+
+const videoRouter = express.Router();
+ 
+
+
+videoRouter.get("/:id(\\d+)", watch);
+videoRouter.get("/:id(\\d+)/edit", getEdit);
+videoRouter.post("/:id(\\d+)/edit", postEdit);
+
+
+
+export default videoRouter;  
