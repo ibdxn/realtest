8d474a66a478e5883f2f3c5cf06effb812ccfdb7
이됴 <iamdy@idyoui-iMac.local>
2022-05-30 15:41:23 +0900

    545444

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,14 +1,14 @@
  
 import express from "express";
-import { edit, remove, logout } from "../controllers/userController";
+import { edit , logout, startGithubLogin, finishGithubLogin } from "../controllers/userController";
 
 const userRouter = express.Router();
 
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
-userRouter.get("/remove", remove);
-
+userRouter.get("/github/start", startGithubLogin);
+userRouter.get("/github/finish", finishGithubLogin);
 
 
 export default userRouter;
