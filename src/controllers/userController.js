c13f8367010f2d8fe4a528683116346d5aaadd78
이됴 <iamdy@idyoui-iMac.local>
2022-06-14 15:43:19 +0900

    ㅋ

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,105 +3,110 @@ import fetch from "node-fetch";
 import bcrypt from "bcrypt"
 
 
-export const getJoin = (req, res) => res.render("join", { pageTitle : "Join"});
-export const postJoin = async(req, res) => {
+export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
+export const postJoin = async (req, res) => {
 
     const { name, username, email, password, password2, location } = req.body;
     const pageTitle = "Join";
-    if (password !== password2){
+    if (password !== password2) {
         return res.status(400).render("join", {
             pageTitle,
             errorMessage: "Password confirmation des not match",
-    });}
+        });
+    }
 
-    const exists = await User.exists({ $or: [{username},{email}]});
+    const exists = await User.exists({ $or: [{ username }, { email }] });
     if (exists) {
         return res.status(400).render("join", {
             pageTitle,
             errorMessage: "This username/email is already taken",
         });
     }
-    
-    try {
-    await User.create({
-        name,
-        username,
-        email,
-        password,
-        location,
 
-    });
-    return res.redirect("/login");
-    } catch(error) {
-        return res.status(400).render("join", {pageTitle: "Join", errorMessage: error._message,});
-            
-        };
+    try {
+        await User.create({
+            name,
+            username,
+            email,
+            password,
+            location,
+
+        });
+        return res.redirect("/login");
+    } catch (error) {
+        return res.status(400).render("join", { pageTitle: "Join", errorMessage: error._message, });
+
+    };
 };
 
-export const getLogin = (req, res)  => res.render("login", { pageTitle: "Login"});
+export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" });
 
-export const postLogin = async(req, res) => {
+export const postLogin = async (req, res) => {
     const { username, password } = req.body;
     const pageTitle = "Login";
     const user = await User.findOne({ username, socialOnly: false });
-    if(!user) {
-        return res.status(400).render("login", {pageTitle, 
-        errorMessage: "An account with this username does not exists.",});
+    if (!user) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "An account with this username does not exists.",
+        });
     };
 
     const ok = await bcrypt.compare(password, user.password)
-    if(!ok){ 
-        return res.status(400).render("login", {pageTitle, 
-        errorMessage: "Wrong password",});
+    if (!ok) {
+        return res.status(400).render("login", {
+            pageTitle,
+            errorMessage: "Wrong password",
+        });
     }
     req.session.loggedIn = true;
     req.session.user = user;
     console.log("LOG USER IN! COMING SOON!");
-     // check if password correct  
+    // check if password correct  
     return res.redirect("/");
 };
 
 export const startGithubLogin = (req, res) => {
-     const baseUrl = `https://github.com/login/oauth/authorize`;
-     const config = {
+    const baseUrl = `https://github.com/login/oauth/authorize`;
+    const config = {
         client_id: process.env.GH_CLIENT,
         allow_signup: false,
         scope: "read:user user:email",
-
-     };
-     
-     const params = new URLSearchParams(config).toString();
-     const finalUrl = `${baseUrl}?${params}`;
-
-     return res.redirect(finalUrl)
-
-
-
-};
-
-export const finishGithubLogin = async(req, res) => {
-
-    const baseUrl = "https://github.com/login/oauth/access_token";
-    const config = {
-        client_id: process.env.GH_CLIENT,
-        client_secret: process.env.GH_SECRET,
-        code: req.query.code, 
 
     };
 
     const params = new URLSearchParams(config).toString();
     const finalUrl = `${baseUrl}?${params}`;
-    const tokenRequest =  await (
-         await fetch(finalUrl, {
-        method:"POST",
-        headers:{
-            Accept : "application/json"
-        },
 
-    })
+    return res.redirect(finalUrl)
+
+
+
+};
+
+export const finishGithubLogin = async (req, res) => {
+
+    const baseUrl = "https://github.com/login/oauth/access_token";
+    const config = {
+        client_id: process.env.GH_CLIENT,
+        client_secret: process.env.GH_SECRET,
+        code: req.query.code,
+
+    };
+
+    const params = new URLSearchParams(config).toString();
+    const finalUrl = `${baseUrl}?${params}`;
+    const tokenRequest = await (
+        await fetch(finalUrl, {
+            method: "POST",
+            headers: {
+                Accept: "application/json"
+            },
+
+        })
     ).json();
 
-    if("access_token" in tokenRequest) {
+    if ("access_token" in tokenRequest) {
 
         const { access_token } = tokenRequest;
         const apiUrl = "https://api.github.com"
@@ -110,7 +115,7 @@ export const finishGithubLogin = async(req, res) => {
                 headers: {
                     Authorization: `token ${access_token}`,
 
-                }, 
+                },
 
             })
         ).json();
@@ -118,13 +123,13 @@ export const finishGithubLogin = async(req, res) => {
         console.log(userData);
 
         const emailData = await (
-        await fetch(`${apiUrl}/user/emails`, {
-            headers: {
-                Authorization: `token ${access_token}`,
+            await fetch(`${apiUrl}/user/emails`, {
+                headers: {
+                    Authorization: `token ${access_token}`,
 
-            }, 
+                },
 
-        })
+            })
 
         ).json();
 
@@ -132,34 +137,33 @@ export const finishGithubLogin = async(req, res) => {
             (email) => email.primary === true && email.verified === true
 
         );
-        if(!emailObj){
+        if (!emailObj) {
             return res.redirect("/login");
         }
         let user = await User.findOne({ email: emailObj.email });
-        if(!user) {
-             await User.create( { 
-                
-            avatarUrl:userData.avatar_url,
-            name:userData.name,
-            username:userData.login,
-            email:emailObj.email,
-            password:"",
-            socialOnly:true,
-            location:userData.location,
-        });         
+        if (!user) {
+            await User.create({
+
+                avatarUrl: userData.avatar_url,
+                name: userData.name,
+                username: userData.login,
+                email: emailObj.email,
+                password: "",
+                socialOnly: true,
+                location: userData.location,
+            });
         }
         req.session.loggedIn = true;
         req.session.user = user;
-        return res.redirect("/");  
+        return res.redirect("/");
 
     } else {
         return res.redirect("/login");
     };
-    
+
 };
 
 
-export const edit = (req, res) => res.send("Edit User");
 
 export const logout = (req, res) => {
 
@@ -167,4 +171,48 @@ export const logout = (req, res) => {
     return res.redirect("/")
 
 };
-export const see =(req, res) => res.send("See"); 
+
+export const getEdit = (req, res) => {
+
+    return res.render("edit-profile", { pageTitle: "Edit Profile" });
+}
+
+export const postEdit = async (req, res) => {
+
+    const {
+        session: {
+            user: { _id, email: originalEmail, username: orignalUserName },
+        },
+        body: { name, email, username, location },
+    } = req;
+
+
+
+    if (email != originalEmail || username != orignalUserName) {
+
+
+        const updatedUser = await User.findByIdAndUpdate(_id, {
+            name,
+            email,
+            username,
+            location,
+        }, { new: true });
+
+        req.session.user = updatedUser;
+        return res.redirect("/users/edit");
+
+    } else {
+        return res.status(400).render("edit-profile", {
+            //pageTitle,
+            errorMessage: "This username/email is already taken",
+        });
+    }
+
+};
+
+
+
+
+
+export const see = (req, res) => res.send("See");
+
