68597b8c0bdbf1bf2e9d9dcdecbf9564a8d76021
이됴 <iamdy@idyoui-iMac.local>
2022-06-30 23:15:03 +0900

    a

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,16 +1,30 @@
  
 import express from "express";
-import { postEdit, getEdit , logout, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword } from "../controllers/userController";
-import {avatarUploade, protectorMiddleware, publicOnlyMiddleware, uploadFiles} from "../middlewares";
+import { postEdit, getEdit , logout, startGithubLogin, finishGithubLogin, getChangePassword, postChangePassword, see } from "../controllers/userController";
+import {avatarUploade, protectorMiddleware, publicOnlyMiddleware, } from "../middlewares";
 
 const userRouter = express.Router();
 
 
-userRouter.get("/logout", protectorMiddleware, logout);
-userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(avatarUploade.single("avatar"),postEdit);
-userRouter.route("/change-password").all(protectorMiddleware).get(getChangePassword).post(postChangePassword);
-userRouter.get("/github/start",publicOnlyMiddleware, startGithubLogin);
-userRouter.get("/github/finish", publicOnlyMiddleware,finishGithubLogin);
+userRouter
+    .get("/logout", protectorMiddleware, logout);
+userRouter
+    .route("/edit")
+    .all(protectorMiddleware)
+    .get(getEdit)
+    .post(avatarUploade.single("avatar"),postEdit);
+userRouter
+    .route("/change-password")
+    .all(protectorMiddleware)
+    .get(getChangePassword)
+    .post(postChangePassword);
+userRouter
+    .get("/github/start",publicOnlyMiddleware, startGithubLogin);
+userRouter
+    .get("/github/finish", publicOnlyMiddleware,finishGithubLogin);
+
+userRouter
+    .get("/:id", see);
 
 
 export default userRouter;
