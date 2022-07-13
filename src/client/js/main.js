8116ea329848672e0872dd7a56a5eaea523345f7
이됴 <iamdy@idyoui-iMac.local>
2022-07-13 22:28:30 +0900

    ㄴ
    ㄴ

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,4 +1,5 @@
 import regeneratorRuntime from "regenerator-runtime";
+//async , await 쓰게 도와주는함수
 import "../scss/styles.scss"; 
  
 
--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -2,9 +2,55 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async() => {
+let stream;
+let recorder;
+let videoFile;
 
-    const stream = await navigator.mediaDevices.getUserMedia({
+const handleDownload = () => {
+
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "MyRecording.webm";
+    document.body.appendChild(a);
+    a.click();
+
+}
+
+const handleStop = () => {
+
+    startBtn.innerText = "Download Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleDownload);
+
+    recorder.stop();
+}
+
+const handleStart = () => {
+
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (event) => {
+        console.log(event.data);
+
+        videoFile = URL.createObjectURL(event.data);
+        video.srcObject = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
+
+    };
+
+    recorder.start();
+
+};
+
+
+const init = async () => {
+
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: false,
         video: true,
 
@@ -13,9 +59,12 @@ const handleStart = async() => {
     video.srcObject = stream;
     video.play();
 
-     
+
 
 
 };
+
+init();
+
 
 startBtn.addEventListener("click", handleStart);
