a0f98117eae28cf3eeb739f15f48fdfd49d7865c
Doyeon <ibdxn@naver.com>
2022-07-24 13:05:59 +0900

    edit

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -143,7 +143,8 @@ const handleEnded = () => {
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
 volumeRange.addEventListener("input", handleVolumeChange);
-video.addEventListener("loadeddata", handleLoadedMetadata);
+video.addEventListener("canplay", handleLoadedMetadata);
+handleLoadedMetadata();
 video.addEventListener("timeupdate", handleTimeUpdate);
 video.addEventListener("click", playCheck);
 document.addEventListener("keydown", spacePlayCheck);
