28e804bb027fdfc08fb169c6caaa65693ad76649
Doyeon <ibdxn@naver.com>
2022-07-27 16:41:08 +0900

    s

--- src/middlewares.js
+++ src/middlewares.js
@@ -65,5 +65,5 @@ export const avatarUpload = multer({
 export const videoUpload = multer({
     dest: "uploads/videos/", limits: {
         fileSize: 10000000,
-    }, storage: isHeroku ?s3VideoUploader : undefined,
+    }, storage: isHeroku ? s3VideoUploader : undefined,
 });
